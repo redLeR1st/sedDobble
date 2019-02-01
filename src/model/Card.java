0550b7c96e2ebdd2a93d1895e11adb54bc3aca49
Irinyi Kabinet Magyar Nyelvű Felhasználó <valaki@pc21724-3.inf.u-szeged.hu>
2019-02-01 15:08:44 +0100

    Modeified the model and contoller

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,9 +1,38 @@
 package model;
 
 public class Card {
-	List<int> simbols;
+	List<int> symbols;
 	
 	Card() {
-		simbols = new ArrayList<int>;
+		symbols = new ArrayList<int>;
+	}
+	
+	void addSymb(int sim) {
+		symbols.add(sim);
+	}
+	
+	
+	boolean match(int i){
+		if (this.symbols.contains(i)) {
+			return true;
+		}
+		else false;	
+	}
+	
+	List<int> getSymbols() {
+		return symbols;
+	}
+	
+	int getSymbolByIndex(int i) {
+		symbols.get(i);
+	}
+	
+	boolean equals(Card c) {
+		for (int i = 0; i < 8; i++) {
+			if (this.symbols.get(i) != c.getSymbolByIndex(i)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -4,7 +4,31 @@ public class User {
 	String name = "";
 	List<Card> hand;
 	
+	public String getUsername() {
+		return name;
+	}
+	
+	public void setName(String un) {
+		name = un;
+	}
+	
 	User() {
 		name = "peldaBela";
 	}
+	
+	Card getNextCard() {
+		return hand.get(hand.size() - 1);
+	}
+	
+	//TODO: WARNING!
+	void setHand(List<Card> hand) {
+		this.hand = hand;
+	}
+	
+	Card dropCard() {
+		Card ret hand.get(hand.size() - 1);
+		hand.remove(ret);
+		return ret;
+	}
+	
 }
